28674bdbd255e1092d9ad2cc4385e86a06f8d13f
Trigun Joshi <trigunjoshi@gmail.com>
2021-11-22 18:48:16 -0800

    assertEqual solved

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+    return true;
+  } else {
+    console.log(`Assertion Failed: [${actual}] === [${expected}]`);
+    return false;
+  }
+};
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1, 1);
