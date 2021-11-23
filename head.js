57ccd9acbfa6bb5db0432cf6db31e470cd5243bc
Trigun Joshi <trigunjoshi@gmail.com>
2021-11-22 19:10:33 -0800

    new solution completed

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+    return true;
+  } else {
+    console.log(`Assertion Failed: [${actual}] === [${expected}]`);
+    return false;
+  }
+};
+
+const head = (arr) => arr[0];
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
