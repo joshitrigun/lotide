668f4c7b9309854c0168171b2e2c0d5039fe8d07
Trigun Joshi <trigunjoshi@gmail.com>
2021-11-24 16:42:35 -0800

    count.js completed

--- assertEqual.js
+++ assertEqual.js
@@ -3,13 +3,12 @@ const assertEqual = (actual, expected) => {
     console.log(`Assertion Passed: [${actual}] === [${expected}]`);
     return true;
   } else {
-    console.log(`Assertion Failed: [${actual}] === [${expected}]`);
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
     return false;
   }
 };
 /*
 assertEqual("Lighthouse labs", "Bootcamp");
 assertEqual(1, 1);
-
 */
 module.exports = assertEqual;
---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = require("./assertEqual");
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = [];
+  console.log(allItems);
+  console.log(itemsToCount);
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      /*
+      if (results[item]) {
+        results[item]++;
+      } else {
+        results[item] = 1;
+      } */
+      !results[item] ? (results[item] = 1) : (results[item] += 1);
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhannxxa"], undefined);
